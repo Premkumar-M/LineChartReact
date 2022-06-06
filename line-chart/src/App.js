712635332e0d0c9619ef1096585c39eecc71cda5
PK <mpremkumarkml@gmail.com>
2022-06-06 16:55:11 +0530

    Initial graph developed

---
+++ line-chart/src/App.js
@@ -0,0 +1,68 @@
+import {LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer, CartesianGrid, Legend} from 'recharts';
+import './App.css';
+
+function App() {
+  const data = [
+    {
+      "name": "Page A",
+      "uv": 4000,
+      "pv": 2400,
+      "amt": 2400
+    },
+    {
+      "name": "Page B",
+      "uv": 3000,
+      "pv": 1398,
+      "amt": 2210
+    },
+    {
+      "name": "Page C",
+      "uv": 2000,
+      "pv": 9800,
+      "amt": 2290
+    },
+    {
+      "name": "Page D",
+      "uv": 2780,
+      "pv": 3908,
+      "amt": 2000
+    },
+    {
+      "name": "Page E",
+      "uv": 1890,
+      "pv": 4800,
+      "amt": 2181
+    },
+    {
+      "name": "Page F",
+      "uv": 2390,
+      "pv": 3800,
+      "amt": 2500
+    },
+    {
+      "name": "Page G",
+      "uv": 3490,
+      "pv": 4300,
+      "amt": 2100
+    }
+  ]
+  return (
+    <div className="App">
+      <h1 className='headerH1'>Report POC</h1>
+      <ResponsiveContainer  width={730} height={250} >
+        <LineChart data={data}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+          <CartesianGrid vertical = {false} strokeDasharray="3 3" />
+          <XAxis dataKey="name"  tickLine={false}  />
+          <YAxis tickLine={false} />
+          <Tooltip cursor={false}/>
+          {/* <Legend /> */}
+          <Line type="" dataKey="pv" stroke="#8884d8" activeDot={true} />
+          <Line type="monotone" dataKey="uv" stroke="#82ca9d"/>
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
+
+export default App;
